89f7346652fca754adef1c9ac0daa14f5f70205e
ShilinZhu <slzhu.cug@gmail.com>
2022-01-28 12:41:02 +0800

    提交

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -41,38 +41,35 @@ namespace AE_practice
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(101, 13);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Location = new System.Drawing.Point(76, 10);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(296, 23);
+            this.comboBox1.Size = new System.Drawing.Size(223, 20);
             this.comboBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 16);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(10, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "选择文件";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(189, 87);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(142, 70);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "确定";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(297, 87);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(223, 70);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "取消";
             this.button2.UseVisualStyleBackColor = true;
@@ -81,24 +78,27 @@ namespace AE_practice
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 54);
+            this.label2.Location = new System.Drawing.Point(10, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.Size = new System.Drawing.Size(53, 12);
             this.label2.TabIndex = 4;
             this.label2.Text = "保存路径";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(101, 51);
+            this.textBox1.Location = new System.Drawing.Point(76, 41);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(296, 25);
+            this.textBox1.Size = new System.Drawing.Size(223, 21);
             this.textBox1.TabIndex = 5;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(403, 50);
+            this.button3.Location = new System.Drawing.Point(302, 41);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(35, 23);
+            this.button3.Size = new System.Drawing.Size(26, 20);
             this.button3.TabIndex = 6;
             this.button3.Text = "…";
             this.button3.UseVisualStyleBackColor = true;
@@ -106,9 +106,9 @@ namespace AE_practice
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 129);
+            this.ClientSize = new System.Drawing.Size(338, 103);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -116,7 +116,6 @@ namespace AE_practice
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
--- Form2.cs
+++ Form2.cs
@@ -1,4 +1,6 @@
 ﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geodatabase;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -44,6 +46,46 @@ namespace AE_practice
                 foldPath = dialog.SelectedPath;
             }
             textBox1.Text = foldPath;
+        }
+        private static string getLayerPath(ILayer pLayer)
+        {
+            IDataLayer2 dataLayer = pLayer as IDataLayer2;
+            IDatasetName dataset = dataLayer.DataSourceName as IDatasetName;
+            IWorkspaceName workspace = dataset.WorkspaceName;
+            string s = workspace.PathName + pLayer.Name;
+            return s;
+        }
+
+        private ILayer GetLayer(IMapControlDefault mapcontrol, string LayerName)
+        {
+            ILayer layer = null;
+            if (LayerName != null)
+            {
+                for (int i = 0; i < mapcontrol.LayerCount; i++)//遍历所有图层
+                {
+                    if (mapcontrol.get_Layer(i).Name == LayerName)
+                    {
+                        layer = mapcontrol.get_Layer(i);
+                    }
+                }
+            }
+            return layer;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string lyrName = comboBox1.Text;
+            string savePath = textBox1.Text;
+            IMapControlDefault Mapcontrol = form1.axMapControl1.Object as IMapControlDefault;           
+            if (lyrName != "" && savePath != "")
+            {
+                ILayer layer = GetLayer(Mapcontrol, lyrName);
+                string path = getLayerPath(layer);
+                //MessageBox.Show(path);
+            }
+            else
+            {
+                MessageBox.Show("请完成参数选择！");
+            }
         }
     }
 }
