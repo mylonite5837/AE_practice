99952d59dec54c623d1d2007fef8bc1086236ec1
朱世林 <@LAPTOP-0959UTOU>
2022-01-28 10:31:54 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.SpatialAnalyst;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AE_practice
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 form1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axTOCControl1.SetBuddyControl(axMapControl1);
+        }
+
+        private void axMapControl1_OnMouseMove(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseMoveEvent e)
+        {
+            toolStripStatusLabel1.Text = string.Format("{0},{1}  {2}", e.mapX.ToString("#######.##"), e.mapY.ToString("#######.##"), axMapControl1.MapUnits.ToString().Substring(4));
+        }
+
+        private void 盆域阴影法ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new Form2(this);
+            form.ShowDialog();
+        }
+
+        private void 文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,49 @@
+﻿using ESRI.ArcGIS.Carto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AE_practice
+{
+    public partial class Form2 : Form
+    {
+        private Form1 form1;
+        public Form2(Form1 form)
+        {
+            InitializeComponent();
+            form1 = form;
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < form1.axMapControl1.Map.LayerCount; i++)
+            {
+                string layerName = form1.axMapControl1.get_Layer(i).Name;
+                comboBox1.Items.Add(layerName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string foldPath = null;
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                foldPath = dialog.SelectedPath;
+            }
+            textBox1.Text = foldPath;
+        }
+    }
+}
